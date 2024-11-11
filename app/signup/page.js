47825b1e2f6db16b940ad9deb07fea0bfb528da4
CodeGallantX <johnayobami77@proton.me>
2024-11-11 22:48:31 +0100

    ./.

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/link';
+import Image from 'next/image';
 const App = () => {
   const [formData, setFormData] = useState({
     firstname: '',
