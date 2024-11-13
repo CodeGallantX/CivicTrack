21be3024e37ceb27144f6faa2506a3a56b8ba805
CodeGallantX <johnayobami77@proton.me>
2024-11-13 18:47:16 +0100

    - Added LICENSE - If...else for project status

--- app/components/PortfolioList.js
+++ app/components/PortfolioList.js
@@ -25,21 +25,34 @@ const PortfolioList = ({ portfolios }) => {
           <div className="p-4 bg-gradient-to-tl from-gray-800 via-pink-700 to-purple-800  mt-4 rounded-lg">
             <h4 className="text-xl font-semibold text-white">Other Projects</h4>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-              {portfolio.otherProjects.map((project, idx) => (
-                <div key={idx} className="bg-gray-800 rounded-lg overflow-hidden shadow-md">
-                  <img
-                    src={project.img}
-                    alt={project.title}
-                    className="w-full h-32 object-cover rounded-t-lg"
-                  />
-                  <div className="p-4">
-                    <h5 className="font-semibold text-lg">{project.title}</h5>
-                    <p className="text-xs mt-2">{project.status}</p>
-                    <p className="mt-2 text-sm text-gray-300">{project.impact}</p>
+              {portfolio.otherProjects.map((project, idx) => {
+                // Conditional status class based on project status
+                let statusClass = '';
+                if (project.status === 'Completed') {
+                  statusClass = 'text-green-500'; // Success theme
+                } else if (project.status === 'Ongoing') {
+                  statusClass = 'text-yellow-500'; // Warning theme
+                } else if (project.status === 'Failed' || project.status === 'Cancelled') {
+                  statusClass = 'text-red-500'; // Danger theme
+                }
+
+                return (
+                  <div key={idx} className="bg-gray-800 rounded-lg overflow-hidden shadow-md">
+                    <img
+                      src={project.img}
+                      alt={project.title}
+                      className="w-full h-32 object-cover rounded-t-lg"
+                    />
+                    <div className="p-4">
+                      <h5 className="font-semibold text-lg">{project.title}</h5>
+                      <p className={`text-xs mt-2 ${statusClass}`}>{project.status}</p>
+                      <p className="mt-2 text-sm text-gray-300">{project.impact}</p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
             </div>
+
           </div>
         </div>
       ))}
