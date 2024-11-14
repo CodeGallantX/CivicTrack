cd6f986ec2568a3020bf010a27c705e8ba43fa2a
John A. Samuel <johnayobami77@proton.me>
2024-11-14 21:33:38 +0100

    Updates

--- app/components/PortfolioList.js
+++ app/components/PortfolioList.js
@@ -1,39 +1,31 @@
+import { useEffect } from 'react';
+import aos from 'aos/dist/aos.css';
+import AOS from 'aos';
+
+
 const PortfolioList = ({ portfolios }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 600,
+      once: true
+    });
+  });
+
   return (
     <div className="w-full">
       {portfolios.map((portfolio) => (
         <div key={portfolio.id} className="overflow-hidden rounded-lg shadow-lg">
-          {/* <div className="relative">
-            <img
-              src={portfolio.img}
-              alt={portfolio.title}
-              className="w-full h-56 object-cover rounded-t-lg"
-            />
-          </div>
-
-          <div className="p-4 text-white">
-            <h3 className="text-xl font-semibold">{portfolio.title}</h3>
-            <p className="text-sm mt-2">{portfolio.description}</p>
-            <p className="mt-2">
-              <strong>Status:</strong> {portfolio.status}
-            </p>
-            <p className="mt-2">
-              <strong>Impact:</strong> {portfolio.impact}
-            </p>
-          </div> */}
-
-          <div className="p-4 bg-gradient-to-tl from-gray-800 via-pink-700 to-purple-800  mt-4 rounded-lg">
-            <h4 className="text-xl font-semibold text-white">Other Projects</h4>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+          <div data-aos="flip-right" className="p-4 bg-gradient-to-tl from-gray-800 via-pink-700 to-purple-800  mt-4 rounded-lg">
+            <h4 data-aos="fade-down" data-aos-duration="2000" className="text-xl font-semibold text-white">Other Projects</h4>
+            <div data-aos="fade-up" data-aos-duration="3000" className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
               {portfolio.otherProjects.map((project, idx) => {
-                // Conditional status class based on project status
                 let statusClass = '';
                 if (project.status === 'Completed') {
-                  statusClass = 'text-green-500';
+                  statusClass = 'text-green-500 bg-green-50 rounded-full p-2';
                 } else if (project.status === 'Ongoing') {
-                  statusClass = 'text-yellow-500';
+                  statusClass = 'text-yellow-500 bg-yellow-50 rounded-full p-2';
                 } else if (project.status === 'Failed' || project.status === 'Cancelled') {
-                  statusClass = 'text-red-500';
+                  statusClass = 'text-red-500 bg-red-50 rounded-full p-2';
                 }
 
                 return (
@@ -44,9 +36,9 @@ const PortfolioList = ({ portfolios }) => {
                       className="w-full h-32 object-cover rounded-t-lg"
                     />
                     <div className="p-4">
-                      <h5 className="font-semibold text-lg">{project.title}</h5>
-                      <p className={`text-xs mt-2 ${statusClass}`}>{project.status}</p>
-                      <p className="mt-2 text-sm text-gray-300">{project.impact}</p>
+                      <h5 data-aos="fade-right" data-aos-duration="3000" className="font-semibold text-lg">{project.title}</h5>
+                      <p data-aos="fade-right" data-aos-duration="3000" className={`text-xs mt-2 ${statusClass}`}>{project.status}</p>
+                      <p data-aos="fade-right" data-aos-duration="3000" className="mt-2 text-sm text-gray-300">{project.impact}</p>
                     </div>
                   </div>
                 );
